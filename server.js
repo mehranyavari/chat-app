5045c2c1cb8e6b3878e658eccabd78baecd3d97a
mehran yavari <mehranyavari@yahoo.com>
2021-05-05 20:07:20 +0430

    config socket

--- server.js
+++ server.js
@@ -3,3 +3,20 @@ const express = require('express');
 const app = express();
 
 const server = require('http').createServer(app);
+
+
+const io = require('socket.io')(server, {
+    cors: { origin: "*" }
+});
+
+
+io.on('connection', (socket) => {
+    console.log('conection');
+    socket.on('disconnect', (socket) => {
+        console.log('disconnect');
+    })
+})
+
+server.listen(3000, () => {
+    console.log('server is run');
+})
