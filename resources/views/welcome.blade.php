b23cc889e0ba1bc6123921f11ac0dc2d07692faa
mehran yavari <mehranyavari@yahoo.com>
2021-05-05 20:31:54 +0430

    added socket js cdn to welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,6 +14,8 @@
 
 <body>
 
+
+<script src="https://cdn.socket.io/4.0.1/socket.io.min.js" integrity="sha384-LzhRnpGmQP+lOvWruF/lgkcqD+WDVt9fU3H4BWmwP5u5LTmkUGafMcpZKNObVMLU" crossorigin="anonymous"></script>
 </body>
 
 </html>
